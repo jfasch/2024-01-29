744a7f7babdcf087004829441a2b1f7eb0c17daa
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-01-29 13:46:07 +0100

    first half day

---
+++ livehacking/args.py
@@ -0,0 +1,6 @@
+import sys
+
+print(sys.argv)
+
+erstes_argument = sys.argv[1]
+print(erstes_argument)
---
+++ livehacking/hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import sys
+
+names = sys.argv[1:]
+
+# print(names)
+
+for name in names:
+    print('jessas, noch einer!')
+    print('hello', name)
